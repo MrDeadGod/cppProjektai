7770e60a24d2be277799f2d71d880111d1a48290
MrDeadGod <82360254+MrDeadGod@users.noreply.github.com>
2024-11-03 22:14:41 +0200

    Update filedir.cpp

--- filedir.cpp
+++ filedir.cpp
@@ -7,13 +7,18 @@ namespace fs = std::filesystem;
 
 int main()
 {
-    std::string dir = "C:\\Users\\Admin\\Desktop";
+    std::string dir = "C:\\Users\\Admin\\Documents\\";
     std::cout << "The files of " << dir << " directory are:" << std::endl << std::endl;
 
     GETFILES(dir) {
             std::cout << "Path: " << entry.path() << std::endl;
             std::cout << "Name: " << entry.path().filename() << std::endl;
-            std::cout << "Size: " << (entry.file_size() / 1000) << " MB" << std::endl;
+            if (entry.is_regular_file()) {
+                    std::cout << "Size: " << std::setprecision(2) << (entry.file_size() / 1024.0 / 1024.0) << " MB" << std::endl;
+            } else {
+                    std::cout << "Size: N/A, it may be because it isn't a regular file" << std::endl;
+            }
+            std::cout << std::endl;
     }   
     return 0;
 }
